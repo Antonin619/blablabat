2519d541a5c77d4f00bf8267201c71a05c80a31e
abourain <antonin.bourain@univ-lr.fr>
2023-01-30 13:13:49 +0100

    router added

---
+++ src/pages/Panel.js
@@ -0,0 +1,16 @@
+import '../App.scss';
+import '../style/main.scss'
+
+function Panel() {
+  return (
+    <div className="Panel">
+      <header className="Panel-header">
+        <p>
+          Panel
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Panel;
