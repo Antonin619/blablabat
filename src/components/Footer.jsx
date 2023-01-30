55ec25e542f589e01ec50eb9311a57884c038342
abourain <antonin.bourain@univ-lr.fr>
2023-01-30 19:21:23 +0100

    wip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,24 @@
 import { Link } from 'react-router-dom';
 import '../style/footer.scss'
+import logo from '../assets/images/logo.svg'
 
 function Footer() {
     return (
       <footer className="footer">
-        <div className="footer-content">
-        <Link className='link' to="/mentions-legales">Mentions Légales</Link>
+          <div className="footer-header">
+            <Link to="/"><img src={logo} alt="logo blablabat" /></Link>
+            <h3>BLABLABAT</h3>
+            
         </div>
+        <div className="footer-content">
+              <h4>Nos solutions</h4>
+              <Link to={'/'}><p>Clients</p></Link>
+              <Link to={'/'}><p>Professionnels</p></Link>
+              <h4>Nos solutions</h4>
+              <Link to={'/'}><p>Qui sommes-nous ?</p></Link>
+              <Link to={'/'}><p>Comment ça marche ?</p></Link>
+              <Link to={'/'}><p>CGV et Mentions légales</p></Link>
+            </div>
       </footer>
     );
   }
