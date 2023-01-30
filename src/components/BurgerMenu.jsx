6f8fe735932dd4e80f440bff09bc364b7281d9c6
abourain <antonin.bourain@univ-lr.fr>
2023-01-30 17:30:10 +0100

    wip

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,19 +1,26 @@
 import '../style/burger_menu.scss'
 import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom';
 
-function BurgerMenu() {
+function BurgerMenu({
+    isEnabled=false,
+    onClose
+}) {
+
+
     return (
-      <header className="header">
-        <div className="header-leftside">
-          <img src={logo} alt="logo blablabat" />
-          <h1>BLABLABAT</h1>
+      <div className={isEnabled ? "burger-menu" : "burger-menu hidden"}>
+        <header className="burger-header">
+            <Link onClick={onClose} to="/"><img src={logo} alt="logo blablabat" /></Link>
+            <i onClick={onClose} className="fa-solid fa-xmark"></i>
+        </header>
+        <div className="burger-content">
+            <Link onClick={onClose} to="/register-pro" className='burger-pro_link'><h4>Vous êtes un professionnel ?</h4></Link>
+            <hr />
+            <Link onClick={onClose} to="/login" className='burger-link'><h4>S'inscrire</h4></Link>
+            <Link onClick={onClose} to="/register" className='burger-link'><h4>Se connecter</h4></Link>
         </div>
-        
-        <div className="header-rightside">
-          <i class="fa-solid fa-bars"></i>
-        </div>
-        
-      </header>
+      </div>
     );
   }
   
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,24 +1,42 @@
 import '../App.scss';
 import '../style/landing_page.scss'
+import illu1 from '../assets/images/illu-1.jpg'
 
 function LandingPage() {
   return (
     <div className="landing-page">
       <header className="landing-page-header">
-        <p>
     
           <h1>Faites de vos projets une réalité...</h1>
-          <div className="test">
-            <p>De quoi avez vous besoin ?</p>
+          <div className="landing-page-input">
+            <div className="icon-container">
+            <i class="fa-solid fa-hammer"></i>
+            </div>
+          
+            <select className='selector' name="" id="">
+              <option>De quoi avez vous besoin ?</option>
+              <option>Peinture</option>
+              <option>Maçonnerie</option>
+              <option>Plomberie</option>
+            </select>
           </div>
-          <div className="test">
-            <p>Code Postal</p>
+          <div className="landing-page-input">
+          <div className="icon-container">
+            <i className="fa-solid fa-location-dot"></i>
+            </div>
+            <select className='selector' name="" id="">
+              <option>Code postal...</option>
+              <option>17000 (Charente-Maritime)</option>
+              <option>44000 (Loire-Atlantique)</option>
+              <option>33000 (Gironde)</option>
+            </select>
           </div>
-          <div className="testt">
-            <p>Rechercher</p><i class="fa-solid fa-magnifying-glass"></i>
+          <div className="landing-page-research">
+            <p>Rechercher</p><i className="fa-solid fa-magnifying-glass"></i>
           </div>
-        </p>
       </header>
+
+      <img src={illu1} alt="test" />
     </div>
   );
 }
