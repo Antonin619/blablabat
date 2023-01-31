4c4169dddc76953b1d16d3320b6a211f24531857
abourain <antonin.bourain@univ-lr.fr>
2023-01-31 11:00:01 +0100

    wip

--- src/pages/Archives.jsx
+++ src/pages/Archives.jsx
@@ -3,13 +3,19 @@ import '../style/archives.scss'
 import logo from '../assets/images/logo.svg'
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
 
 function Archives() {
     useEffect(() => {
         window.scrollTo(0, 0)
       }, [])
+  let history = useNavigate();
+
   return (
     <div className="archives">
+        <Link to="/"> <button onClick={() => history(-1)} className='back-button' type="button"><i className='fa-solid fa-arrow-left'></i>Retour</button></Link>
+
         <h1>Archives</h1>
 
         <div className="archives-cards">
@@ -27,9 +33,9 @@ function Archives() {
 
             <div className="archives-card">
                                 <div className="archives-card-header">
-                                    <h3>Richard Dubois</h3>
-                                    <p className="subject">Isolation</p>
-                                    <p className="date">18/07/2022</p>
+                                    <h3>Isabelle Yohan</h3>
+                                    <p className="subject">Toiture</p>
+                                    <p className="date">28/10/2022</p>
                                     <p className="location">17000, La Rochelle</p>
                                 </div>
                                 <div className="archives-card-footer cancel">
@@ -39,9 +45,9 @@ function Archives() {
 
             <div className="archives-card">
                                 <div className="archives-card-header">
-                                    <h3>Richard Dubois</h3>
+                                    <h3>François Hollande</h3>
                                     <p className="subject">Isolation</p>
-                                    <p className="date">18/07/2022</p>
+                                    <p className="date">10/12/2022</p>
                                     <p className="location">17000, La Rochelle</p>
                                 </div>
                                 <div className="archives-card-footer success">
@@ -50,9 +56,9 @@ function Archives() {
             </div>
             <div className="archives-card">
                                 <div className="archives-card-header">
-                                    <h3>Richard Dubois</h3>
-                                    <p className="subject">Isolation</p>
-                                    <p className="date">18/07/2022</p>
+                                    <h3>Thierry Deveaud</h3>
+                                    <p className="subject">Peinture</p>
+                                    <p className="date">08/06/2022</p>
                                     <p className="location">17000, La Rochelle</p>
                                 </div>
                                 <div className="archives-card-footer cancel">
